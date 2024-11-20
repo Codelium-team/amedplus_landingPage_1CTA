5b9bc838724e5efa3e13c298ca0716fda386097d
Hector Garcia <garcia27h@hotmail.com>
2024-11-20 12:56:09 -0300

    feat: Se agregaron modales en categorias y se añadieron estilos a la landing

--- js/main.js
+++ js/main.js
@@ -63,3 +63,17 @@ document.querySelectorAll('.nav-link').forEach(link => {
         }
     });
 });
+
+document.querySelectorAll('.direction').forEach(link => {
+    link.addEventListener('click', function (e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href');
+        const targetElement = document.querySelector(targetId);
+        if (targetElement) {
+            targetElement.scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+        }
+    });
+});
