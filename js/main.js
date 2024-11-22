291d4700a603a72c76ff308916aa753c8ec11e3f
Hector Garcia <garcia17h@gmail.com>
2024-11-22 13:31:49 -0300

    Feat: Arreglando problema de las ramas

--- js/main.js
+++ js/main.js
@@ -1,37 +1,6 @@
-/* const swiper = new Swiper('.swiper', {
-
-    direction: 'horizontal',
-    loop: true,
-
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-
-    slidesPerView: 1,
-    spaceBetween: 20,
-
-
-    breakpoints: {
-        620: {
-            slidesPerView: 1,
-            spaceBetween: 20,
-        },
-        680: {
-            slidesPerView: 2,
-            spaceBetween: 20,
-        },
-        920: {
-            slidesPerView: 3,
-            spaceBetween: 20,
-        },
-    }
-}); */
-
 const swiper = new Swiper(".mySwiper", {
     spaceBetween: 20,
     loop:true,
-    /* centeredSlides: true, */
     
     pagination: {
         el: ".swiper-pagination",
@@ -50,7 +19,6 @@ const swiper = new Swiper(".mySwiper", {
             slidesPerView: 3,
             spaceBetween: 20,
         },
-       /*  slidesPerView: 3,  */
     },
     navigation: {
         nextEl: '.swiper-button-next',
@@ -81,3 +49,31 @@ const swiper = new Swiper(".mySwiper", {
 
     form.submit();
   };
+
+  document.querySelectorAll('.nav-link').forEach(link => {
+    link.addEventListener('click', function (e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href');
+        const targetElement = document.querySelector(targetId);
+        if (targetElement) {
+            targetElement.scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+        }
+    });
+});
+
+document.querySelectorAll('.direction').forEach(link => {
+    link.addEventListener('click', function (e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href');
+        const targetElement = document.querySelector(targetId);
+        if (targetElement) {
+            targetElement.scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+        }
+    });
+});
