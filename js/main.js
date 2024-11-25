1118f3548854a3fbddbdee41a2e87e13f1f4e5ea
Hector Garcia <garcia27h@hotmail.com>
2024-11-25 12:48:54 -0300

    Feat: Correcciones de UX/UI, arregledo redireccion de modales

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 const swiper = new Swiper(".mySwiper", {
     spaceBetween: 20,
-    loop:true,
-    
+    loop: true,
+
     pagination: {
         el: ".swiper-pagination",
         clickable: true,
@@ -24,33 +24,33 @@ const swiper = new Swiper(".mySwiper", {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
     },
-  });
+});
 
-  const validacon = (event) => {
+const validacion = (event) => {
     event.preventDefault();
-  
+
     const form = event.target;
     console.log(form)
     const name = form.name.value.trim();
     const email = form.email.value.trim();
     const subject = form.subject.value.trim();
     const message = form.message.value.trim();
-  
+
     if (!name || !email || !subject || !message) {
-      alert('Por favor, completa todos los campos.');
-      return false;
+        alert('Por favor, completa todos los campos.');
+        return false;
     }
 
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (!emailRegex.test(email)) {
-      alert('Por favor, introduce un email válido.');
-      return false;
+        alert('Por favor, introduce un email válido.');
+        return false;
     }
 
     form.submit();
-  };
+};
 
-  document.querySelectorAll('.nav-link').forEach(link => {
+document.querySelectorAll('.nav-link').forEach(link => {
     link.addEventListener('click', function (e) {
         e.preventDefault();
         const targetId = this.getAttribute('href');
@@ -74,6 +74,31 @@ document.querySelectorAll('.direction').forEach(link => {
                 behavior: 'smooth',
                 block: 'start'
             });
+        }
+    });
+});
+
+document.querySelectorAll('.btn.btn-dark.desp').forEach(button => {
+    button.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href');
+        const targetElement = document.querySelector(targetId);
+
+        const modalElement = this.closest('.modal');
+        const modal = bootstrap.Modal.getInstance(modalElement);
+
+        if (modal) {
+            modal.hide();
+            modalElement.addEventListener('hidden.bs.modal', () => {
+
+                if (targetElement) {
+                    targetElement.scrollIntoView({
+                        behavior: 'smooth',
+                        block: 'start',
+                    });
+                }
+            }, { once: true });
         }
     });
 });
